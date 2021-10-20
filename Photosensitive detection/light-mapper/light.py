34d4a3f6f5dea99bb3646793e05200c2d0b73773
a732264856 <71765455+a732264856@users.noreply.github.com>
2021-10-20 09:22:09 +0800

    Add files via upload
    You need a Raspberry Pi 3b+, a photosensitive sensor, and a kubeedge cluster. Activate the photosensitive sensor on the edge of the Raspberry Pi and feed back information to the cloud. The program to start the photosensitive sensor and connect to evevtbus is a python type program.

---
+++ Photosensitive detection/light-mapper/light.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# encoding:utf-8
+import json
+import sys
+import os
+import paho.mqtt.client as mqtt
+import time
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+
+TASK_TOPIC = "$hw/events/device/" + "light" + "/twin/update"
+
+client_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+
+client = mqtt.Client(client_id, transport='tcp')
+
+client.connect("192.168.1.204", 1883, 60)
+client.loop_start()
+
+
+def clicent_main(message: str):
+
+    time_now = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
+    payload = {"event_id":"","timestamp":0,"twin":{"light-status":{"actual": {"value": "%s"%message}, "metadata": {"type":"Updated"}}}}
+
+    client.publish(TASK_TOPIC, json.dumps(payload, ensure_ascii=False))
+
+    return True
+
+pin_pqrs=24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_pqrs, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+try:
+    while True:
+        status = GPIO.input(pin_pqrs)
+        if status == False:
+            print('1')
+            clicent_main('light')
+        else:
+            print('0')
+            clicent_main('no_light')
+        time.sleep(0.5)
+except KeyboradInterrupt:
+    GPIO.cleanup()
+
